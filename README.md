00ec132eaeafd4f0089bd9389a64dd105f5630ce
BottelAlexis <bottel.alexis@hotmail.com>
2022-11-03 13:43:18 +0100

    UPDATE: do not spawn on snake

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # SNAKE GAME js
 
 ## Context
+
 It's a project I put in place to practice JavaScript, HTML, CSS.  
-I first tried it using div's, than I stumbled accross the HTML canvas element, and a good explanation from 'CodeExplainedRepo' (in references)
+I first tried it using div's, than I stumbled across the HTML canvas element, and a good explanation from 'CodeExplainedRepo' (in references)
 
 ## Try it
+
 You can try this repo [here](https://htmlpreview.github.io/?https://github.com/BOAScripts/SnakeGame-JS-Canvas/blob/main/index.html)
 
-> ## ToDo's  
-> - [ ] game over screen
-> - [ ] replayability (without refreshing the page)
-> - [ ] keep high score of session
-> - [ ] add some sound effect
-> - [ ] do not spawn goal on snake
-> - [ ] spawn "enemy" blocks 
-> - [ ] make it prettier :smile:
+## ToDo's  
+
+- [ ] game over screen
+- [ ] replayability (without refreshing the page)
+- [ ] keep high score of session
+- [ ] add some sound effect
+- [x] do not spawn goal on snake
+- [ ] spawn "enemy" blocks 
+- [ ] make it prettier :smile:
 
 > ### References
 > https://github.com/CodeExplainedRepo/Snake-JavaScript  
--- src/script.js
+++ src/script.js
@@ -5,6 +5,11 @@ let width = document.querySelector('#width'); // input
 const grid = document.querySelector('#grid'); // canvas 
 const ctx = grid.getContext("2d"); // canvas context
 
+let scoreDisplay = document.querySelector('#score'); // text
+const btnSection = document.querySelector('.btnSection'); // Section
+const playBtn = document.querySelector('#play'); // button
+const resetBtn = document.querySelector('#reset'); // button
+
 const cell = 32; // cell size in px
 const bgCell = new Image();
 bgCell.src = "src/img/snakeBg.png" // Background cell image
@@ -15,17 +20,12 @@ snakeBody.src = "src/img/snakeBody.png" // Body of snake image
 const goalImg = new Image();
 goalImg.src = "src/img/goal.png" // Goal image
 
-let scoreDisplay = document.querySelector('#score'); // text
-
-const playBtn = document.querySelector('#play'); // button
-const resetBtn = document.querySelector('#reset'); // button
-
-
 let maxX; // Grid boundaries
 let maxY;
+const gridArray = []; // Array of all pos in grid {x:..,y:..}
 
-let snake = []; // array of obj{x:...,y:...}
-let goal = {}; // obj {}{x:...,y:...}
+let snake = []; // array of objs{x:..,y:..}
+let goal = {}; // obj {}{x:..,y:..}
 let score = 0;
 let d = "RIGHT"; // init direction is right
 
@@ -34,14 +34,18 @@ let canPress = true; // key press timing mitigation
 
 // START ON PLAY
 playBtn.addEventListener('click', () => {
-    playBtn.disabled = true;
-    playBtn.innerText = "...PLAYING...";
+    //btnSection.style.display = 'none';
     // Set init. grid and boundaries
     generateGrid(width.value,height.value);
+    getGridArray(maxX,maxY);
     // set init. cells of the snake (middle of grid)
     generateStartSnake(maxX,maxY);
     // set init. goal
-    generateNextGoal(maxX,maxY);
+    const initGoalPos = getAvailablePos();
+    goal = {
+        x : initGoalPos.x,
+        y : initGoalPos.y
+    }
     // start the game
     game = setInterval(displayFrame,150);
     // get direction input
@@ -58,8 +62,7 @@ function displayFrame(){
     // redraw grid
     generateGrid(maxX,maxY);
     // display goal
-    ctx.drawImage(goalImg,goal.x,goal.y);
-
+    ctx.drawImage(goalImg,goal.x*cell,goal.y*cell);    
     // draw snake
     for (let body of snake){
         ctx.drawImage(snakeBody,body.x*cell,body.y*cell);
@@ -84,10 +87,15 @@ function displayFrame(){
     }
 
     // when snake hits a goal
-    if (headX === (goal.x / cell) && headY === (goal.y / cell)){
+    if (headX === goal.x && headY === goal.y){
         score++;
         scoreDisplay.innerText = score;
-        generateNextGoal(maxX,maxY);
+        const newGoalPos = getAvailablePos();
+        goal = {
+            x : newGoalPos.x,
+            y : newGoalPos.y
+        }
+        //console.log(`goalX:${goal.x/cell}`,`goalY:${goal.y/cell}`)
     }
     else {
         snake.pop();
@@ -107,28 +115,60 @@ function displayFrame(){
     snake.unshift(newHead);
     // Allow again an input
     canPress = true;
-
-
+    
 }
+
 // Draw grid
 function generateGrid (width,height) {
     if (width<10 && height<10){
-        width = 30;
-        height = 20;
+        width = 11;
+        height = 11;
     }
-    // define grid size
+    // define boundaries of canvas
     grid.width = width * cell;
     grid.height = height * cell;
-    // add each cell
+    // draw each cell
     for (let i=0; i<width; i++){
         for (let j=0; j<height; j++){
             ctx.drawImage(bgCell,i*cell,j*cell);
         }
     }
+    // set value of global vars
     maxX = width;
     maxY = height;
     //return `${width}-${height}`;
 }
+
+function getGridArray (gridMaxX,gridMaxY){
+    for (let k=0; k<gridMaxX; k++){
+        for (let l=0; l<gridMaxY; l++){
+            gridArray.push({x : k, y : l})
+        }
+    }
+}
+
+// return a random position where snake isn't
+function getAvailablePos(){
+    const gridNotSnake = [].concat(gridArray);
+    for (let n in snake){
+        for (let m in gridNotSnake){
+            // condition seems not OK :-(
+            if (snake[n].x == gridNotSnake[m].x && snake[n].y == gridNotSnake[m].y){
+                gridNotSnake.splice(m,1);
+            }
+        }
+    }
+    
+    if (gridArray.length !== (gridNotSnake.length + snake.length)){
+        console.log(gridNotSnake, snake, `grid =${gridArray.length}; grid-snake =${gridNotSnake.length + snake.length}`);
+        gameOver();
+        console.error ('DIF ERROR')
+    }
+
+    let availablePos = gridNotSnake[Math.floor(Math.random() * gridNotSnake.length)];
+    return availablePos;
+}
+
 // Define init snake position in grid
 function generateStartSnake(maxX,maxY){
     snake[0] = {
@@ -141,13 +181,7 @@ function generateStartSnake(maxX,maxY){
     }
 
 }
-// Define goal position in grid
-function generateNextGoal(maxX,maxY){
-    goal = {
-        x : Math.floor(Math.random()*maxX) * cell,
-        y : Math.floor(Math.random()*maxY) * cell
-    }
-}
+
 // Define direction on key press
 function setDirection(event){
     let key = event.code;
